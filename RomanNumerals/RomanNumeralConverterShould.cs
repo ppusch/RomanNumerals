6734d53e0b1f99d035ce05b19f6f36a6c5daa33c
Philippe <Philippe@DESKTOP-3T3ABSL>
2016-04-26 12:38:45 +0200

    Unit tests Refactored

--- RomanNumerals/RomanNumeralConverterShould.cs
+++ RomanNumerals/RomanNumeralConverterShould.cs
@@ -10,84 +10,21 @@ namespace RomanNumerals
 {
     public class RomanNumeralConverterShould
     {
-        [Test]
-        public void return_I_when_Arabic_Number_Is_1()
+        [TestCase(1,"I")]
+        [TestCase(2,"II")]
+        [TestCase(3,"III")]
+        [TestCase(4,"IV")]
+        [TestCase(5,"V")]
+        [TestCase(6,"VI")]
+        [TestCase(7,"VII")]
+        [TestCase(8,"VIII")]
+        [TestCase(9,"IX")]
+        [TestCase(10,"X")]
+        public void convert_to_roman_number(int arabicNumber, string expectedRomanNumber)
         {
             var romanNumeralConverter = new RomanNumeralConverter();
 
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(1)).Equals("I");
-        }
-
-        [Test]
-        public void return_II_when_Arabic_Number_Is_2()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(2)).Equals("II");
-        }
-
-        [Test]
-        public void return_III_when_Arabic_Number_Is_3()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(3)).Equals("III");
-        }
-
-        [Test]
-        public void return_IV_when_Arabic_Number_Is_4()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(4)).Equals("IV");
-        }
-
-        [Test]
-        public void return_V_when_Arabic_Number_Is_5()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(5)).Equals("V");
-        }
-
-        [Test]
-        public void return_VI_when_Arabic_Number_Is_6()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(6)).Equals("VI");
-        }
-
-        [Test]
-        public void return_VII_when_Arabic_Number_Is_7()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(7)).Equals("VII");
-        }
-
-        [Test]
-        public void return_VIII_when_Arabic_Number_Is_8()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(8)).Equals("VIII");
-        }
-
-        [Test]
-        public void return_IX_when_Arabic_Number_Is_9()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(9)).Equals("IX");
-        }
-
-        [Test]
-        public void return_X_when_Arabic_Number_Is_10()
-        {
-            var romanNumeralConverter = new RomanNumeralConverter();
-
-            Check.That(romanNumeralConverter.ConvertToRomanNumber(10)).Equals("X");
+            Check.That(romanNumeralConverter.ConvertToRomanNumber(arabicNumber)).Equals(expectedRomanNumber);
         }
 
 
